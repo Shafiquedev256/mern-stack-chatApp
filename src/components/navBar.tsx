05d75a47391c90325bdc782885894052b1fbfdca
Musinguzi shafiq <shafiquedev256@gmail.com>
2024-01-23 21:01:31 +0300

    change ui

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -1,16 +1,12 @@
 import chatIcone from "../assets/chat-dots-svgrepo-com.svg"
-import settingIcone from "../assets/setting-5-svgrepo-com.svg"
 import groupIcone from "../assets/group-svgrepo-com.svg" 
-import profileIcone from "../assets/profile-svgrepo-com.svg" 
 
 export const Navbar = () => {
   return (
-    <div className="bg-white  p-4 fixed bottom-0 w-screen left-0">
+    <div className="bg-gray-100  p-4 fixed bottom-0 w-screen left-0">
         <div className="flex flex-row justify-evenly items-center w-screen">
-          <img src={settingIcone} className="text-gray-300 hover:bg-blue-200 hover:rounded hover:bg-opacity-60 p-1" />
           <img src={chatIcone} className="text-gray-300 hover:bg-blue-200 hover:rounded hover:bg-opacity-60 p-1" />
           <img src={groupIcone} className="text-gray-300 hover:bg-blue-200 hover:rounded hover:bg-opacity-60 p-1" />
-          <img src={profileIcone} className="text-gray-300 hover:bg-blue-200 hover:rounded hover:bg-opacity-60 p-1" />
       </div>
     </div>
   );
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,22 +3,22 @@ import imgEl from "../assets/IMG-20231216-WA0009.jpg"
  const HomePage = ()=>{
    return (
      <> 
-     <section className="bg-gray-300  py-2">
+     <section className="py-2">
      <div className="text-2xl font-light p-4 text-gray-900">Messages</div>  
-     <input className="w-60 bg-white p-3 rounded  border-0 mx-3" placeholder="search friend..."/> 
-     <div className="flex flex-row overflow-scroll w-screen p-2">
+     <input className="w-60 bg-gray-100 outline-0 p-3 rounded  border-0 ml-2" placeholder="search friend..."/> 
+     <div className="flex flex-row overflow-scroll  p-2">
     <div className="bg-white rounded-lg p-2"> 
     
-    <div className="relative rounded-full w-16 h-16">
+    <div className="relative rounded-full w-16 h-16 flex flex-col justify-center">
       <img
         src={imgEl}
-        className="w-16 h-16 rounded-full object-center"
+        className="w-16 h-16 rounded-full object-cover"
         alt="img"
       /> 
-      <div className="bg-green-500 w-4 h-4 rounded-full absolute -bottom-2 -right-2 border-2 border-white"></div> 
+      <div className="bg-green-500 w-4 h-4 rounded-full absolute  bottom-0 left-0 border-2 border-white"></div> 
       </div>
       
-      <p className="text-sm font-semibold">Shafiq</p>
+      <p className="text-xs font-semibold">Shafiq</p>
     </div>
      </div>
      </section>
